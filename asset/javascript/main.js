0070795c24e2578cc9752d885a53c33f81017e7c
DenuccioDiego <diegodenuccio95@gmail.com>
2021-10-15 17:25:16 +0200

    prova ciclo non funzioante

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -22,16 +22,46 @@ const text = [
      'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ];
 
-const arrayvar = items[0] 
+let contatore = 0
+const segnaIndice = items[contatore] 
 const selezione = document.getElementById("container_immagine_grande")
-console.log(arrayvar)
-console.log(selezione) 
-
-selezione.insertAdjacentHTML("afterbegin", `<div class="immagine_principale" style="background-image: url('./asset/img/${arrayvar}') ;" ></div>`) 
+//console.log(arrayvar)
+//console.log(selezione) 
 
 
+selezione.insertAdjacentHTML("afterbegin", `<div class="immagine_principale" style="background-image: url('./asset/img/${segnaIndice}') ;" ></div>`) 
 
+for(contatore=0; contatore<=4 ; contatore++){
 
+     
+     document.querySelector(".fa-chevron-circle-up").addEventListener("click", frecciaSuperiore) 
+     document.querySelector(".fa-chevron-circle-down").addEventListener("click", frecciaInferiore) 
+     
+     function frecciaSuperiore(){
+          if (contatore=0){
+               contatore=4
+          }
+          else {
+               contatore--
+          }
+     }
+
+     function frecciaInferiore(){
+          if (contatore=4){
+                contatore=0
+          }
+
+          else {
+                contatore++  
+          }
+     }
+
+     
+     document.querySelector(".immagine_principale").style.backgroundImage="url('./asset/img/${segnaIndice}')"
+
+}
+     
+     
 
 
 
